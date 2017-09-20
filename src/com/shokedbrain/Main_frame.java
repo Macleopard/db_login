d187735ff116004bb0dc18ce0f015c75c5be2228
Andrew <a.solomencev@yandex.ru>
2017-09-20 21:09:01 +0300

    Initial commit

---
+++ src/com/shokedbrain/Main_frame.java
@@ -0,0 +1,13 @@
+package com.shokedbrain;
+
+import javax.swing.*;
+
+public class Main_frame {
+
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("db_login");
+
+    }
+}
